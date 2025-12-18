48305cd779bd538fe5196cbc802bfa5c892e8ab5
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2025-12-18 17:42:01 -0400

    Corrigir base path para /bee-experiences/

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/bee-experience/',
+  base: '/bee-experiences/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
