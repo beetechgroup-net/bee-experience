085f6ab730d9382e5044bcdb051fefa868193271
Gabriel Menezes <bolsista.senaiemp18@ms.senai.br>
2025-12-23 15:30:57 -0400

    Vite config for new dns

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: '/bee-experience/',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
